1735607bb467ba5dce3274e6d29a277d9600e0f1
Tinfoil-droid <137796947+Tinfoil-droid@users.noreply.github.com>
2023-07-27 18:03:07 +0100

    added resize button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 function createGrid() {
     const container = document.getElementById('container');
 
@@ -38,3 +36,29 @@ window.onload = () => {
     resetBoard();
 };
 
+function canvasSize() {
+    const chooseSize = document.getElementById('prompt');
+    chooseSize.addEventListener('click', () => {
+      const userInput = window.prompt('Type pad size (max. 100)', '');
+      let userSize = parseInt(userInput, 10);
+      if (userSize === '' || isNaN(userSize)) return;
+      userSize = Math.min(userSize, 100);
+      resizeGrid(userSize); 
+    });
+  }
+  
+  function resizeGrid(userSize) {
+    const container = document.getElementById('container');
+    container.innerHTML = ''; 
+  
+    for (let i = 0; i < userSize; i++) {
+      for (let j = 0; j < userSize; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        container.appendChild(grid);
+        draw();
+      }
+    }
+  }
+  
+  canvasSize();
