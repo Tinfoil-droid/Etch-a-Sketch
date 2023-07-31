37d48b2cd02fee91f802e61e04b5adc9db5f4008
Tinfoil-droid <137796947+Tinfoil-droid@users.noreply.github.com>
2023-07-31 18:45:31 +0100

    added random comments

--- scripts.js
+++ scripts.js
@@ -29,22 +29,11 @@ function canvasSize() {
         else {
         container.innerHTML = '';
         createGrid(userSize);
+        talkbox.innerHTML = '';
+        setTimeout(displayComment, 5000);
         } 
     });
 }
-
-//Old, unoptimized, code:
-//The forEach(item) added individual event listeners for each grid cell, which slowed things down.
-
-// function draw() {
-//     const canvas = document.querySelectorAll('.grid');
-
-//     canvas.forEach(item => {
-//         item.addEventListener('mouseover', () => {
-//             item.style.backgroundColor = 'black';
-//         });
-//     });
-// };
 
 function draw() {
     const container = document.getElementById('container');
@@ -53,6 +42,8 @@ function draw() {
         if (event.target.classList.contains('grid')) {
             event.target.style.backgroundColor = 'black';
         }
+    
+        
     });
 }
 
@@ -63,6 +54,8 @@ function resetBoard() {
         container.innerHTML = '';
         createGrid(32);
         draw();
+        talkbox.innerHTML = '';
+        setTimeout(displayComment, 5000);
     });
 }
 
@@ -73,24 +66,33 @@ window.onload = () => {
     resetBoard();
 };
 
+let comments = [
+    'Wow. Never seen that before.',
+    'You\'re a regular Picasso.',
+    'How very original.',
+    'Well. That is certainly...something.',
+    'Now this is fridge door worthy.',
+    'I know I\'m just a piece of code but... I forgot where I was going with this.',
+    'If Michelangelo could see this, he would probably cry.',
+    'What is this even supposed to be?',
+    'Comment library depleted. Reloading comment library...',
+    'I don\'t know what I\'m seeing, I\'m just a stupid piece of code.',
+    'I don\'t know. Ask me again later.',
+    'Yes. That is ... Wow.',
+    'No. Just no.',
+    'What a horrible day to have eyes.',
+    'What do you mean, you want colors? Guy who wrote this couldn\'t even be bothered to align the buttons properly.',
+    'That is the most beautiful thing I have ever seen.',
+    'Somebody get the Louvre on the phone.',
+    'What am I even looking at here?'
+]
 
-// Old code:
-// When i first wrote this, I just copied the above function.
-// Now, I merge them so that a single function can both create the og board as well as resize it.
+function displayComment() {
+  let index = Math.floor(Math.random() * comments.length);
+  let comment = comments[index];
+  let element = document.getElementById('talkbox');
+  element.innerHTML = comment;
+  element.style.fontSize = '4ch';
+}
 
-//   function resizeGrid(userSize) {
-//     const container = document.getElementById('container');
-//     container.innerHTML = ''; 
-
-//     container.style.gridTemplateColumns = `repeat(${userSize}, 1fr)`;
-//     container.style.gridTemplateRows = `repeat(${userSize}, 1fr)`;
-  
-//     for (let i = 0; i < userSize; i++) {
-//       for (let j = 0; j < userSize; j++) {
-//         const grid = document.createElement('div');
-//         grid.classList.add('grid');
-//         container.appendChild(grid);
-//         draw();
-//       }
-//     }
-//   }
+setTimeout(displayComment, 5000);
